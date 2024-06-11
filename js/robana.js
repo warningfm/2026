ea6452c487130bbf94c4b127d18c06f8da3b6321
Radio Warning FM <warningfm@gmail.com>
2024-06-11 21:44:08 +0700

    Update robana.js

--- js/robana.js
+++ js/robana.js
@@ -274,32 +274,29 @@ function getStreamingData() {
 // Variável global para armazenar as músicas
 var audio = new Audio(URL_STREAMING);
 
+
 // Player control
-class Player {
-    constructor() {
-        this.play = function () {
-            audio.play();
+function Player() {
+    this.play = function () {
+        audio.play();
 
-            var defaultVolume = document.getElementById('volume').value;
+        var defaultVolume = document.getElementById('volume').value;
 
-            if (typeof (Storage) !== 'undefined') {
-                if (localStorage.getItem('volume') !== null) {
-                    audio.volume = intToDecimal(localStorage.getItem('volume'));
-                } else {
-                    audio.volume = intToDecimal(defaultVolume);
-                }
+        if (typeof (Storage) !== 'undefined') {
+            if (localStorage.getItem('volume') !== null) {
+                audio.volume = intToDecimal(localStorage.getItem('volume'));
             } else {
                 audio.volume = intToDecimal(defaultVolume);
             }
-            document.getElementById('volIndicator').innerHTML = defaultVolume;
-            
-            togglePlay(); // Adiciona esta linha para atualizar o botão
-        };
+        } else {
+            audio.volume = intToDecimal(defaultVolume);
+        }
+        document.getElementById('volIndicator').innerHTML = defaultVolume;
+    };
 
-        this.pause = function () {
-            audio.pause();
-        };
-    }
+    this.pause = function () {
+        audio.pause();
+    };
 }
 
 // On play, change the button to pause
@@ -308,7 +305,7 @@ audio.onplay = function () {
     var bplay = document.getElementById('buttonPlay');
     if (botao.className === 'fa fa-play') {
         botao.className = 'fa fa-pause';
-        bplay.firstChild.data = 'PAUSAR';
+        bplay.firstChild.data = 'PAUSE';
     }
 }
 
@@ -323,9 +320,24 @@ audio.onpause = function () {
 }
 
 // Unmute when volume changed
+// audio.onvolumechange = function () {
+//     if (audio.volume > 0) {
+//         audio.muted = false;
+//     }
+// }
+// Unmute when volume changed
 audio.onvolumechange = function () {
     if (audio.volume > 0) {
-        audio.muted = false;
+        audio.muted = false;        
+        var botmute = document.getElementById('playerMute');
+        if (botmute.className === 'fa fa-volume-off') {
+            botmute.className = 'fa fa-volume-up';
+        }
+    }else{
+        var botmute = document.getElementById('playerMute');
+        if (botmute.className === 'fa fa-volume-up') {
+            botmute.className = 'fa fa-volume-off';
+        }
     }
 }
 
@@ -337,31 +349,27 @@ audio.onerror = function () {
     }
 }
 
-document.getElementById('volume').oninput = function () {
-    audio.volume = intToDecimal(this.value);
+setTimeout(function(){
+    document.getElementById('volume').oninput = function () {
+        audio.volume = intToDecimal(this.value);    
+        var page = new Page();
+        page.changeVolumeIndicator(this.value);
+    }
+    document.getElementById("triggerToggle").addEventListener('click', function() {
+        slideToggle(document.getElementById("historicSong"), 400);
+    });
 
-    var page = new Page();
-    page.changeVolumeIndicator(this.value);
-}
+}, 3000);
 
 
 function togglePlay() {
-    const playerButton = document.getElementById("playerButton");
-    const isPlaying = playerButton.classList.contains("fa-pause-circle");
-  
-    if (isPlaying) {
-      playerButton.classList.remove("fa-pause-circle");
-      playerButton.classList.add("fa-play-circle");
-      playerButton.style.textShadow = "0 0 5px black";
-      audio.pause();
+    if (!audio.paused) {
+        audio.pause();
     } else {
-      playerButton.classList.remove("fa-play-circle");
-      playerButton.classList.add("fa-pause-circle");
-      playerButton.style.textShadow = "0 0 5px black";
-      audio.load();
-      audio.play();
+        audio.load();
+        audio.play();
     }
-  }
+}
 
 function volumeUp() {
     var vol = audio.volume;
@@ -382,72 +390,226 @@ function volumeDown() {
 }
 
 function mute() {
+    // togglePlay()
     if (!audio.muted) {
         document.getElementById('volIndicator').innerHTML = 0;
         document.getElementById('volume').value = 0;
         audio.volume = 0;
         audio.muted = true;
+
+        var botmute = document.getElementById('playerMute');
+        if (botmute.className === 'fa fa-volume-up') {
+            botmute.className = 'fa fa-volume-off';
+        }
     } else {
-        var localVolume = localStorage.getItem('volume');
-        document.getElementById('volIndicator').innerHTML = localVolume;
-        document.getElementById('volume').value = localVolume;
-        audio.volume = intToDecimal(localVolume);
+        // var localVolume = localStorage.getItem('volume');
+        document.getElementById('volIndicator').innerHTML = 50;
+        document.getElementById('volume').value = 50;
+        audio.volume = intToDecimal(50);
         audio.muted = false;
+
+        var botmute = document.getElementById('playerMute');
+        if (botmute.className === 'fa fa-volume-off') {
+            botmute.className = 'fa fa-volume-up';
+        }
     }
 }
 
-document.addEventListener('keydown', function (event) {
-    var key = event.key;
+function getStreamingData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            if (this.response.length === 0) {
+                console.log('%cdebug', 'font-size: 22px');
+            }
+
+            var data = JSON.parse(this.responseText);
+            console.log('Received data:', data); // Add this line for debugging
+
+            var page = new Page();
+
+            // Formating characters to UTF-8
+            let song = data.song ? data.song.replace(/&apos;/g, '\'') : '';
+            let artist = data.artist ? data.artist.replace(/&apos;/g, '\'') : '';
+
+            // Change the title
+            document.title = song + ' - ' + artist + ' | ' + RADIO_NAME;
+
+            if (document.getElementById('currentSong').innerHTML !== song) {
+                page.refreshCover(song, artist);
+                page.refreshCurrentSong(song, artist);
+                page.refreshLyric(song, artist);
+
+                for (var i = 0; i < 2; i++) {
+                    page.refreshHistoric(data.history[i], i);
+                }
+            }
+        }
+    };
+
+    var d = new Date();
+
+    // Requisition with timestamp to prevent cache on mobile devices
+    xhttp.open('GET', API_URL);
+    xhttp.send();
+}
+
+// Player control by keys
+document.addEventListener('keydown', function (k) {
+    var k = k || window.event;
+    var key = k.keyCode || k.which;
+    
     var slideVolume = document.getElementById('volume');
+
     var page = new Page();
 
     switch (key) {
         // Arrow up
-        case 'ArrowUp':
+        case 38:
             volumeUp();
             slideVolume.value = decimalToInt(audio.volume);
             page.changeVolumeIndicator(decimalToInt(audio.volume));
             break;
         // Arrow down
-        case 'ArrowDown':
+        case 40:
             volumeDown();
             slideVolume.value = decimalToInt(audio.volume);
             page.changeVolumeIndicator(decimalToInt(audio.volume));
             break;
         // Spacebar
-        case ' ':
-        case 'Spacebar':
+        case 32:
             togglePlay();
             break;
         // P
-        case 'p':
-        case 'P':
+        case 80:
             togglePlay();
             break;
         // M
-        case 'm':
-        case 'M':
+        case 77:
             mute();
             break;
-        // Numeric keys 0-9
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            var volumeValue = parseInt(key);
-            audio.volume = volumeValue / 10;
-            slideVolume.value = volumeValue * 10;
-            page.changeVolumeIndicator(volumeValue * 10);
+        // 0
+        case 48:
+            audio.volume = 0;
+            slideVolume.value = 0;
+            page.changeVolumeIndicator(0);
+            break;
+        // 0 numeric keyboard
+        case 96:
+            audio.volume = 0;
+            slideVolume.value = 0;
+            page.changeVolumeIndicator(0);
+            break;
+        // 1
+        case 49:
+            audio.volume = .1;
+            slideVolume.value = 10;
+            page.changeVolumeIndicator(10);
+            break;
+        // 1 numeric key
+        case 97:
+            audio.volume = .1;
+            slideVolume.value = 10;
+            page.changeVolumeIndicator(10);
+            break;
+        // 2
+        case 50:
+            audio.volume = .2;
+            slideVolume.value = 20;
+            page.changeVolumeIndicator(20);
+            break;
+        // 2 numeric key
+        case 98:
+            audio.volume = .2;
+            slideVolume.value = 20;
+            page.changeVolumeIndicator(20);
+            break;
+        // 3
+        case 51:
+            audio.volume = .3;
+            slideVolume.value = 30;
+            page.changeVolumeIndicator(30);
+            break;
+        // 3 numeric key
+        case 99:
+            audio.volume = .3;
+            slideVolume.value = 30;
+            page.changeVolumeIndicator(30);
+            break;
+        // 4
+        case 52:
+            audio.volume = .4;
+            slideVolume.value = 40;
+            page.changeVolumeIndicator(40);
+            break;
+        // 4 numeric key
+        case 100:
+            audio.volume = .4;
+            slideVolume.value = 40;
+            page.changeVolumeIndicator(40);
+            break;
+        // 5
+        case 53:
+            audio.volume = .5;
+            slideVolume.value = 50;
+            page.changeVolumeIndicator(50);
+            break;
+        // 5 numeric key
+        case 101:
+            audio.volume = .5;
+            slideVolume.value = 50;
+            page.changeVolumeIndicator(50);
+            break;
+        // 6 
+        case 54:
+            audio.volume = .6;
+            slideVolume.value = 60;
+            page.changeVolumeIndicator(60);
+            break;
+        // 6 numeric key
+        case 102:
+            audio.volume = .6;
+            slideVolume.value = 60;
+            page.changeVolumeIndicator(60);
+            break;
+        // 7
+        case 55:
+            audio.volume = .7;
+            slideVolume.value = 70;
+            page.changeVolumeIndicator(70);
+            break;
+        // 7 numeric key
+        case 103:
+            audio.volume = .7;
+            slideVolume.value = 70;
+            page.changeVolumeIndicator(70);
+            break;
+        // 8
+        case 56:
+            audio.volume = .8;
+            slideVolume.value = 80;
+            page.changeVolumeIndicator(80);
+            break;
+        // 8 numeric key
+        case 104:
+            audio.volume = .8;
+            slideVolume.value = 80;
+            page.changeVolumeIndicator(80);
+            break;
+        // 9
+        case 57:
+            audio.volume = .9;
+            slideVolume.value = 90;
+            page.changeVolumeIndicator(90);
+            break;
+        // 9 numeric key
+        case 105:
+            audio.volume = .9;
+            slideVolume.value = 90;
+            page.changeVolumeIndicator(90);
             break;
     }
 });
-
 
 function intToDecimal(vol) {
     return vol / 100;
@@ -456,3 +618,93 @@ function intToDecimal(vol) {
 function decimalToInt(vol) {
     return vol * 100;
 }
+
+/* show / hide historic songs */
+let slideUp = (target, duration=500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout( () => {
+      target.style.display = 'none';
+      target.style.removeProperty('height');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+      //alert("!");
+    }, duration);
+  }
+
+  let slideDown = (target, duration=500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+
+    if (display === 'none')
+      display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout( () => {
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+  }
+   let slideToggle = (target, duration = 500) => {
+    if (window.getComputedStyle(target).display === 'none') {
+      return slideDown(target, duration);
+    } else {
+      return slideUp(target, duration);
+    }
+  }
+   
+// ====  
+  
+// let speedAnimation = 400;
+// let targetId = document.getElementById("historicSong");
+
+// let slideBtnClick = (id, sl) => document.getElementById(id).addEventListener('click', () => sl(targetId, speedAnimation));
+
+// slideBtnClick('triggerUp', slideUp);
+// slideBtnClick('triggerDown', slideDown);
+// slideBtnClick('triggerToggle', slideToggle);
+
+
+// =========== old
+
+//   document.getElementById("triggerUp").addEventListener('click', function() {
+//   slideUp(document.getElementById("target"), 400);
+// });
+//   document.getElementById("triggerDown").addEventListener('click', function() {
+//   slideDown(document.getElementById("target"), 400);
+// });
+//   document.getElementById("triggerToggle").addEventListener('click', function() {
+//   slideToggle(document.getElementById("target"), 400);
+// });
